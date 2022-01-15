4e14552793e674dd5a3e4d1eb41c1020b66565c7
Maaz <devmaaaaz@gmail.com>
2022-01-15 23:43:56 +0500

    Switched img from Image ('next/image')

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,4 @@
 import { useTheme } from 'next-themes'
-import Image from 'next/image'
 
 const Hero = () => {
   const { resolvedTheme } = useTheme()
@@ -9,11 +8,10 @@ const Hero = () => {
 
   return (
     <div className="relative max-w-[180px] md:max-w-[272px] md:h-[92px] h-[61px] w-full">
-      <Image
+      <img
         src={`/${resolvedImage}`}
         alt="Google Logo"
-        objectFit="contain"
-        layout="fill"
+        className="object-contain"
       />
       <p className="absolute px-5 text-sm font-bold tracking-wider text-green-500 transform translate-x-full border rounded shadow-lg select-none border-green-500/40 lg:text-base shadow-green-400/10 right-1/4 -top-10">
         Affix
